3de120f8b1779eabc6e4d952f35b9d8563dfc08b
yyyu200 <yyyu200@163.com>
2022-07-05 17:28:12 +0800

    Create cell.py

---
+++ cellmaker/cell.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class cell(object):
+    self.cell=[[1,0,0],[0,1,0],[0,0,1]]
+    self.atpos=[]
+    self.ntyp_name=[]
+    self.ntyp_num=[]
+    self.attyp=[]
+    self.nat=0
+    self.ntyp=0
+
+            
